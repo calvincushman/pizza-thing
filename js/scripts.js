14a7e5209e054ba8d0b29d63454113f06acca3fc
Calvin Cushman <calvincushman@gmail.com>
2017-05-12 11:06:54 -0700

    add collection points for newPizza atributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic/////////////
+
 // constructors
 function Order(pizzas, orderPrice) {
   this.pizzas = [];
@@ -5,26 +7,65 @@ function Order(pizzas, orderPrice) {
 }
 
 
-function Pizza(size, sauce, cheese, toppings, price) {
+function Pizza(size, sauce, crust, cheese, toppings, price) {
   this.size = size;
   this.sauce = sauce;
+  this.crust = crust;
   this.cheese = [];
   this.toppings = [];
   this.price = price;
 }
 
+//prototypes
+
 Order.prototype.newOrder = function() {
-  var newOrder = new Order (pizzas, orderPrice)
+  var newOrder = new Order (pizzas, 0)
 };
 
-Order.prototype.addPizza = function() {
+Order.prototype.addPizza = function(newPizza) {
   this.pizzas.append(newPizza);
 };
 
+Order.prototype.orderPrice = function() {
+  this.orderPrice += newPizza.price
+};
+
 Pizza.prototype.newPizza = function() {
-  var newPizza = new Pizza (size, sauce, cheese, toppings, price)
+  var newPizza = new Pizza (size, sauce, cheese, toppings, 0)
 };
 
 Pizza.prototype.pizzaPrice = function() {
-  this.price = 
-}
+  this.price = size += sauce += cheese += toppings
+};
+
+//user interface////////////
+
+$(function(){
+  // make objects newOrder & newPizza
+  var newOrder = newOrder.newOrder();
+  var newPizza = newPizza.newPizza();
+
+  $("form#addPizza").submit(function(event) {
+    event.preventDefault();
+
+    //grab inputs from form
+    var size = $("#sizeSelection").val();
+
+    var sauce = $("input:radio[sauce=sauce]:checked").val();
+
+    var crust = $("input:radio[crust=crust]:checked").val();
+
+    var cheese = []; $("input:checkbox[name=cheese]:checked").each(function() {
+      var selectedCheese = $(this).val();
+      cheese.push(selectedCheese);
+    });
+
+    var toppings = []; $("input:checkbox[name=toppings]:checked").each(function() {
+      var selectedTopping = $(this).val();
+      toppings.push(selectedTopping);
+    });
+
+    newPizza = (size, sauce, crust, cheese, toppings);
+
+  });
+});
