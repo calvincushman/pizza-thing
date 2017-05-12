4b3c2458e9721079817748f50de5e6b0aa93d896
Calvin Cushman <calvincushman@gmail.com>
2017-05-12 15:42:10 -0700

    get pizza price prototype working properly(git add js/scripts.js)

--- js/scripts.js
+++ js/scripts.js
@@ -4,47 +4,35 @@
 function Order(pizzas, orderPrice) {
   this.pizzas = [];
   this.orderPrice = orderPrice;
-}
+};
 
 
-function Pizza(size, sauce, crust, cheese, toppings, price) {
+function Pizza(size, sauce, crust, cheese, toppings) {
   this.size = size;
   this.sauce = sauce;
   this.crust = crust;
-  this.cheese = [];
-  this.toppings = [];
-  this.price = price;
-}
+  this.cheese = cheese;
+  this.toppings = toppings;
+};
 
 //prototypes
 
-Order.prototype.newOrder = function() {
-  var newOrder = new Order (pizzas, 0)
-};
-
-Order.prototype.addPizza = function(newPizza) {
-  this.pizzas.append(newPizza);
-};
-
 Order.prototype.orderPrice = function() {
-  this.orderPrice += newPizza.price
+  return this.orderPrice + newPizza.price
 };
 
-Pizza.prototype.pizzaPrice = function(price) {
-  var price = sizePrice + cheesePrice + toppingsPrice
+Pizza.prototype.pizzaPrice = function() {
+  return this.size + this.cheese.length + this.toppings.length
 };
-
 //user interface////////////
 
 $(function(){
 
-  $("form#addPizza").submit(function(event) { debugger;
+  $("form#addPizza").submit(function(event) {
     event.preventDefault();
 
     //grab inputs from form
-    var size = $("#sizeSelection").val();
-
-    var sizePrice = parseInt(size);
+    var size = parseInt($("#sizeSelection").val());
 
     var sauce = $("#sauceSelection").val();
 
@@ -55,19 +43,16 @@ $(function(){
       cheese.push(selectedCheese);
     });
 
-    var cheesePrice = parseInt(cheese.length);
-
     var toppings = []; $("input:checkbox[name=toppings]:checked").each(function() {
       var selectedTopping = $(this).val();
       toppings.push(selectedTopping);
     });
 
-    var toppingsPrice = parseInt(toppings.length);
+    var newPizza = new Pizza(size, sauce, crust, cheese, toppings);
 
-    var newPizza = new Pizza(size, sauce, crust, cheese, toppings, price);
+    var price = newPizza.pizzaPrice();
 
-    var price = newPizza.pizzaPrice(sizePrice, cheesePrice, toppingsPrice);
-
+    var newOrder = new Order
     $("ul").append("<li>" + "Size: " + size + " inches" + "<br>" + "Crust: " + crust + "<br>" + "Sauce: " + sauce + "<br>" + "Cheese: " + cheese + "<br>" + "Toppings: " + toppings + "<br>" + "Price: $" + price + "<br>" + "<br>" + "</li>");
   });
 });
